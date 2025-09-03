14def27789c08209171cba02cc4f589b399712e3
dev-celo <marcelohenrique.backend@gmail.com>
2025-09-03 20:59:10 -0300

    deploy vercel

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ const rootElement = document.getElementById('root');
 if (rootElement) {
   createRoot(rootElement).render(
     <StrictMode>
-      <HashRouter basename="/cronograma-ava"> {/* ✅ Adicione o basename aqui */}
+      <HashRouter basename="/"> {/* ✅ Adicione o basename aqui */}
         <App />
       </HashRouter>
     </StrictMode>
